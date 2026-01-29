0cc22d19e4199ef7fec420e40a7d4ac141e38911
virtuserve001-jpg <virtuserve001@gmail.com>
2026-01-30 00:10:27 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,80 +1,33 @@
-const CACHE_NAME = 'partner-ringer-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
+const CACHE = "partner-ringer-v3";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./soft-alarm1.mp3"
 ];
 
-// Install - cache files
-self.addEventListener('install', event => {
-  console.log('[SW] Installing...');
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('[SW] Caching files');
-        return cache.addAll(urlsToCache);
-      })
-      .then(() => {
-        console.log('[SW] Skip waiting');
-        return self.skipWaiting();
-      })
+self.addEventListener("install", e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e=>{
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE && caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
 
-// Activate - claim clients
-self.addEventListener('activate', event => {
-  console.log('[SW] Activating...');
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('[SW] Deleting old cache:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    }).then(() => {
-      console.log('[SW] Claiming clients');
-      return self.clients.claim();
-    })
-  );
+self.addEventListener("notificationclick", e=>{
+  e.notification.close();
+  e.waitUntil(clients.openWindow("./"));
 });
-
-// Fetch - serve from cache or network
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-      .catch(() => {
-        return new Response('Offline');
-      })
-  );
-});
-
-// Notification click
-self.addEventListener('notificationclick', event => {
-  console.log('[SW] Notification click');
-  event.notification.close();
-  
-  event.waitUntil(
-    clients.matchAll({ type: 'window', includeUncontrolled: true })
-      .then(clientList => {
-        for (let client of clientList) {
-          if ('focus' in client) {
-            return client.focus();
-          }
-        }
-        if (clients.openWindow) {
-          return clients.openWindow('./');
-        }
-      })
-  );
-});
-
-console.log('[SW] Service Worker loaded');
